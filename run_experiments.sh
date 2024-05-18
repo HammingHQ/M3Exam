80a4a77e6500edebfd09c08359e360ef854b6335
Sumanyu Sharma <sumanyu@hotmail.com>
2024-05-18 17:05:01 -0500

    Add GPT-4o

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 settings=('zero-shot' 'few-shot')
-models=('azure/gpt-35-turbo' 'azure/gpt-4-turbo' 'groq/Llama3-70b-8192' 'gemini/gemini-pro' 'command-r' 'command-r-plus' 'claude-3-opus-20240229' 'claude-3-sonnet-20240229' 'claude-3-haiku-20240307')
+models=('azure/gpt-35-turbo' 'azure/gpt-4-turbo' 'groq/Llama3-70b-8192' 'gemini/gemini-pro' 'command-r' 'command-r-plus' 'claude-3-opus-20240229' 'claude-3-sonnet-20240229' 'claude-3-haiku-20240307' 'gpt-4o')
 reasoning=('default' 'cot')
 num_samples=1
 
